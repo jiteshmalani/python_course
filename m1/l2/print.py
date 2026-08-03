bb2daea862a52a95f9c3bf36bb2d13a10e707254
jiteshmalani <malanisiddhesh@gmail.com>
2026-08-03 17:56:13 +0530

    l2 activities

---
+++ m1/l2/print.py
@@ -0,0 +1,6 @@
+print("hello world")
+
+# printing a no.
+print(29)
+
+print("my birthday is", 29)
---
+++ m1/l2/variiables.py
@@ -0,0 +1,6 @@
+name = "jitesh"
+age = 14
+print(name)
+print(age)
+name2 = input("what is your name?")
+print("nice to meet you", name2)
