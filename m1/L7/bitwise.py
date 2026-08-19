51ff978cd22d6b94309037d6e11c1dd3613e179c
jiteshmalani <malanisiddhesh@gmail.com>
2026-08-19 17:47:36 +0530

    l7

---
+++ m1/L7/bitwise.py
@@ -0,0 +1,30 @@
+a = 7
+
+b = 3
+
+# and &
+print(a & b)
+
+
+# or |
+print(a | b)
+
+
+# not ~
+print(~ b)
+
+
+# xor ^
+print(a ^ b)
+
+
+# left shift <<
+print(a << b)
+
+
+# right shift >>
+print(a >> b)
+
+
+
+
---
+++ m1/L7/membership.py
@@ -0,0 +1,13 @@
+a = "mango"
+
+b = ["mango", "banana"]
+
+print(a in b)
+
+#---------------
+
+c = "banana"
+
+d = ["mango", "apple"]
+
+print(c not in d)
